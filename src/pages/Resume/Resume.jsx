db5bde4fbd6d70a1a8e3d2e3449719eec423a8ba
Alex Petit <alexpetit6@gmail.com>
2023-11-23 14:44:46 -0800

    links to pdfs in prog

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -12,7 +12,21 @@ export default function Resume() {
         <div className="corner-border" id='corner-border-3'></div>
         <div className="corner-border" id='corner-border-4'></div>
         <div id='resume-container'>
-
+          <div>
+            <h3>Choreography</h3>
+            <a href="https://sei-chum-bucket.s3.us-west-2.amazonaws.com/choreography-resume.pdf" target='_blank' rel="noreferrer">
+              <i className="fa-regular fa-file-pdf" style={{color: '#ffffff'}}></i>
+            </a>
+            <a href="https://sei-chum-bucket.s3.us-west-2.amazonaws.com/choreography-resume.pdf">
+              <button>View</button>
+            </a>
+          </div>
+          <div>
+            <h3>Performance</h3>
+            <a href="https://sei-chum-bucket.s3.us-west-2.amazonaws.com/performance-resume.pdf" target='_blank' rel="noreferrer">
+              <i className="fa-regular fa-file-pdf" style={{color: '#ffffff'}}></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
