44bbceba1e4ce30dc279e13543f570aa9ec07693
Alex Petit <alexpetit6@gmail.com>
2023-11-27 12:48:57 -0800

    made tabs expand content with transition

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,8 +1,16 @@
 import './Accordion.css';
 import Resume from '../../pages/Resume/Resume';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Accordion() {
+  const [closed, setClosed] = useState({
+    about: true,
+    choreo: true,
+    perf: true,
+    resume: true,
+    contact: true,
+  });
+
   const aboutRef = useRef(null);
   const choreoRef = useRef(null);
   const perfRef = useRef(null);
@@ -18,25 +26,30 @@ export default function Accordion() {
   }
 
   function handleExpand(page) {
-    refs[page].current.style.height = 'auto';
+    setClosed({
+      ...closed,
+      [page]: false
+    })
+
+    // refs[page].current.style.height = 'auto';
   }
 
   return (
     <div id='accordion'>
-      <div onClick={() => handleExpand('about')} ref={aboutRef}  className="accordion-tab" id='about-tab'>
+      <div onClick={() => handleExpand('about')} ref={aboutRef}  className={closed.about ? 'closed-tab' : 'open-tab'} id='about-tab'>
         <div className="tab-title"><h1>About</h1></div>
       </div>
-      <div onClick={() => handleExpand('choreo')} ref={choreoRef} className="accordion-tab" id='choreographer-tab'>
+      <div onClick={() => handleExpand('choreo')} ref={choreoRef} className={closed.choreo ? 'closed-tab' : 'open-tab'} id='choreographer-tab'>
         <div className="tab-title"><h1>Choreography</h1></div>
       </div>
-      <div onClick={() => handleExpand('perf')} ref={perfRef} className="accordion-tab" id='performance-tab'>
+      <div onClick={() => handleExpand('perf')} ref={perfRef} className={closed.perf ? 'closed-tab' : 'open-tab'} id='performance-tab'>
         <div className="tab-title"><h1>Performance</h1></div>
       </div>
-      <div onClick={() => handleExpand('resume')} ref={resumeRef} className="accordion-tab" id='resume-tab'>
+      <div onClick={() => handleExpand('resume')} ref={resumeRef} className={closed.resume ? 'closed-tab' : 'open-tab'} id='resume-tab'>
         <div className="tab-title"><h1>Resumes</h1></div>
         <Resume />
       </div>
-      <div onClick={() => handleExpand('contact')} ref={contactRef} className="accordion-tab" id='contact-tab'>
+      <div onClick={() => handleExpand('contact')} ref={contactRef} className={closed.contact ? 'closed-tab' : 'open-tab'} id='contact-tab'>
         <div className="tab-title"><h1>Contact</h1></div>
       </div>
     </div>
