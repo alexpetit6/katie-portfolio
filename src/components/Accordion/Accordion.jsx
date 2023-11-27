6420a0f38cc648ab43362ebb0faeb8e4a2084da2
Alex Petit <alexpetit6@gmail.com>
2023-11-26 17:40:08 -0800

    made an expand function for accordion

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,23 +1,42 @@
 import './Accordion.css';
 import Resume from '../../pages/Resume/Resume';
+import { useRef } from 'react';
 
 export default function Accordion() {
+  const aboutRef = useRef(null);
+  const choreoRef = useRef(null);
+  const perfRef = useRef(null);
+  const resumeRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const refs = {
+    about: aboutRef,
+    choreo: choreoRef,
+    perf: perfRef,
+    resume: resumeRef,
+    contact: contactRef
+  }
+
+  function handleExpand(page) {
+    refs[page].current.style.height = 'auto';
+  }
+
   return (
     <div id='accordion'>
-      <div className="accordion-tab" id='about-tab'>
+      <div onClick={() => handleExpand('about')} ref={aboutRef}  className="accordion-tab" id='about-tab'>
         <div className="tab-title"><h1>About</h1></div>
       </div>
-      <div className="accordion-tab" id='choreographer-tab'>
+      <div onClick={() => handleExpand('choreo')} ref={choreoRef} className="accordion-tab" id='choreographer-tab'>
         <div className="tab-title"><h1>Choreography</h1></div>
       </div>
-      <div className="accordion-tab" id='performance-tab'>
+      <div onClick={() => handleExpand('perf')} ref={perfRef} className="accordion-tab" id='performance-tab'>
         <div className="tab-title"><h1>Performance</h1></div>
       </div>
-      <div className="accordion-tab" id='resume-tab'>
+      <div onClick={() => handleExpand('resume')} ref={resumeRef} className="accordion-tab" id='resume-tab'>
         <div className="tab-title"><h1>Resumes</h1></div>
         <Resume />
       </div>
-      <div className="accordion-tab" id='contact-tab'>
+      <div onClick={() => handleExpand('contact')} ref={contactRef} className="accordion-tab" id='contact-tab'>
         <div className="tab-title"><h1>Contact</h1></div>
       </div>
     </div>
