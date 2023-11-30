24d6b21a3f2df8e10be0628ac778b61c21183341
Alex Petit <alexpetit6@gmail.com>
2023-11-30 15:15:24 -0800

    tab transitions slide seamlessly

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -18,6 +18,12 @@ export default function Accordion() {
   const resumeRef = useRef(null);
   const contactRef = useRef(null);
 
+  const aboutTitleRef = useRef(null);
+  const choreoTitleRef = useRef(null);
+  const perfTitleRef = useRef(null);
+  const resumeTitleRef = useRef(null);
+  const contactTitleRef = useRef(null);
+
   const refs = {
     about: aboutRef,
     choreo: choreoRef,
@@ -26,15 +32,26 @@ export default function Accordion() {
     contact: contactRef
   };
 
+  const titleRefs = {
+    about: aboutTitleRef,
+    choreo: choreoTitleRef,
+    perf: perfTitleRef,
+    resume: resumeTitleRef,
+    contact: contactTitleRef
+  };
+
   function handleExpand(page) {
     const current = refs[page].current;
+    const titleRef = titleRefs[page].current;
 
     setClosed({
       ...closed,
       [page]: 'open-tab'
     });
 
-    current.style.height = `calc(${current.scrollHeight}px - 20vh`;
+    console.log(current.scrollHeight)
+
+    current.style.height = `calc(${current.scrollHeight}px - ${titleRef.scrollHeight}px`;
 
     function scroll() {
       const portPos = refs[page].current.getBoundingClientRect().y;
@@ -50,21 +67,22 @@ export default function Accordion() {
   
   return (
     <div id='accordion'>
-      <div ref={aboutRef}  className={closed.about} id='about-tab'>
-        <div onClick={() => handleExpand('about')} className="tab-title"><h1>About</h1></div>
+      <div ref={aboutRef} className={closed.about} id='about-tab'>
+        <div onClick={() => handleExpand('about')} ref={aboutTitleRef} className="tab-title"><h1>About</h1></div>
+        <About currentRef={refs.about.current} setter={setClosed} closed={closed} page='about' />
       </div>
       <div ref={choreoRef} className={closed.choreo} id='choreographer-tab'>
-        <div onClick={() => handleExpand('choreo')} className="tab-title"><h1>Choreography</h1></div>
+        <div onClick={() => handleExpand('choreo')} ref={choreoTitleRef} className="tab-title"><h1>Choreography</h1></div>
       </div>
       <div ref={perfRef} className={closed.perf} id='performance-tab'>
-        <div onClick={() => handleExpand('perf')} className="tab-title"><h1>Performance</h1></div>
+        <div onClick={() => handleExpand('perf')} ref={perfTitleRef} className="tab-title"><h1>Performance</h1></div>
       </div>
       <div ref={resumeRef} className={closed.resume} id='resume-tab'>
-        <div onClick={() => handleExpand('resume')} className="tab-title"><h1>Resumes</h1></div>
+        <div onClick={() => handleExpand('resume')} ref={resumeTitleRef} className="tab-title"><h1>Resumes</h1></div>
         <Resume currentRef={refs.resume.current} setter={setClosed} closed={closed} />
       </div>
       <div ref={contactRef} className={closed.contact} id='contact-tab'>
-        <div onClick={() => handleExpand('contact')} className="tab-title"><h1>Contact</h1></div>
+        <div onClick={() => handleExpand('contact')} ref={contactTitleRef} className="tab-title"><h1>Contact</h1></div>
       </div>
     </div>
   )
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,7 +2,7 @@ import './Main.css';
 import Landing from '../../components/Landing/Landing';
 import ContactBar from '../../components/ContactBar/ContactBar';
 import Accordion from '../../components/Accordion/Accordion';
-import About from '../About/About';
+// import About from '../About/About';
 
 export default function Main () {
 
@@ -11,7 +11,7 @@ export default function Main () {
     <ContactBar />
     <Landing />
     <Accordion />
-    <About />
+    {/* <About /> */}
     </>
   )
 
