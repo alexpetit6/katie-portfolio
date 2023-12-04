fd885e573d859355910f77c775b1ba94c49993b3
Alex Petit <alexpetit6@gmail.com>
2023-12-04 12:51:52 -0800

    added controller for albums

---
+++ config/upload-file.js
@@ -0,0 +1,14 @@
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const {S3_REGION, S3_BUCKET, S3_BASE_URL} = process.env;
+
+module.exports = async function(file) {
+  const s3Client = new S3Client({ region: S3_REGION});
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: `${Date.now()}-${file.originalname}`,
+    Body: file.buffer
+  };
+  await s3Client.send(new PutObjectCommand(s3Params));
+  console.log(s3Params.Key)
+  return `${S3_BASE_URL}${S3_BUCKET}/${s3Params.Key}`;
+};
---
+++ controllers/api/albums.js
@@ -0,0 +1,81 @@
+const uploadFile = require('../../config/upload-file');
+const Album = require('../../models/album');
+
+module.exports = {
+  index,
+  show,
+  create,
+  update,
+  delete: deleteAlbum,
+};
+
+async function index(req, res) {
+  const albums = await Album.find({}).sort('date').exec();
+  res.json(albums);
+}
+
+async function show(req, res) {
+  try {
+    const album = await Album.findById(req.params.id);
+    res.json(album);
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function create(req, res) {
+  try {
+    for (let key in req.body) {
+      if (req.body[key] === '') delete req.body[key];
+    };
+    const thumbnailURL = await uploadFile(req.files['thumbnail'][0]);
+    const photoURLs = await Promise.all(req.files['photos'].map(async (p) => {
+      return await uploadFile(p);
+    }));
+    const album = await Album.create({
+      title: req.body.title,
+      thumbnail: thumbnailURL,
+      photos: photoURLs,
+    });
+    res.json(album);
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function update(req, res) {
+  try {
+    for (let key in req.body) {
+      if (req.body[key] === '') delete req.body[key];
+    };
+    const updatedAlbum = await Album.findOneAndUpdate(
+      {_id: req.params.id},
+      {title: req.body.title},
+      {new: true}
+    );
+    if (req.files['thumbnail']) {
+      const thumbnailURL = await uploadFile(req.files['thumbnail'][0]);
+      updatedAlbum.thumbnail = thumbnailURL;
+    };
+    if (req.files['photos']) {
+      const newPhotoURLs = await Promise.all(req.files['photos'].map(async (p) => {
+        return await uploadFile(p);
+      }));
+      newPhotoURLs.forEach((p) => updatedAlbum.photos.push(p));
+    };
+    await updatedAlbum.save();
+    res.json(updatedAlbum);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function deleteAlbum(req, res) {
+  try {
+    await Album.deleteOne({_id: req.params.id});
+    const albums = await Album.find({}).sort('date').exec();
+    res.json(albums);
+  } catch (err) {
+    res.json(err);
+  }
+}
