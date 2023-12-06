fb24eae82486d2710ebcddb5ec1e96c25b5a0a9c
Alex Petit <alexpetit6@gmail.com>
2023-12-05 20:00:35 -0800

    can get albums and put them in thumbnails

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -1,4 +1,6 @@
 import './Album.css';
+import { useState, useEffect } from 'react';
+import { getAlbums } from '../../utilities/albums-api';
 import Header from '../Header/Header';
 import Line from '../Line/Line';
 import AlbumThumbnail from '../AlbumThumbnail/AlbumThumbnail';
@@ -6,6 +8,18 @@ import OpenAlbumForm from '../OpenAlbumForm/OpenAlbumForm';
 
 
 export default function Album({title, currentRef, setter, closed, page, user}) {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(function() {
+    async function getAllAlbums() {
+      const allAlbums = await getAlbums();
+      setAlbums(allAlbums)
+    }
+    getAllAlbums();
+  }, [])
+
+  const AlbumThumbnails = albums.map(a => <AlbumThumbnail album={a} key={a.id} setAlbums={setAlbums} user={user} />)
+
   return(
     <div className="album">
       <Header 
@@ -18,10 +32,7 @@ export default function Album({title, currentRef, setter, closed, page, user}) {
       <OpenAlbumForm user={user} />
       <div className="double-line"><Line /><Line /></div>
       <div className="album-thumbnails-container">
-        <AlbumThumbnail />
-        <AlbumThumbnail />
-        <AlbumThumbnail />
-        <AlbumThumbnail />
+        {AlbumThumbnails}
       </div>
     </div>
 
--- src/components/AlbumThumbnail/AlbumThumbnail.jsx
+++ src/components/AlbumThumbnail/AlbumThumbnail.jsx
@@ -1,12 +1,12 @@
 import './AlbumThumbnail.css';
 import Line from '../Line/Line';
 
-export default function AlbumThumbnail() {
+export default function AlbumThumbnail({album, setAlbums, user}) {
   return (
     <div className='album-thumbnail'>
-      <h1>Book of Mormon</h1>
+      <h1>{album.title}</h1>
       <div className='album-thumbnail-img'>
-        <img src="https://i.imgur.com/Rls5q4N.jpg" alt="" />
+        <img src={album.thumbnail} alt="" />
       </div>
       <Line />
     </div>
