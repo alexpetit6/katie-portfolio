6d4bb1d44ee0930a384986587aa1ef5157754ea1
Alex Petit <alexpetit6@gmail.com>
2023-12-05 19:48:14 -0800

    form clears after submitting and button disables

--- src/components/NewAlbumForm/NewAlbumForm.jsx
+++ src/components/NewAlbumForm/NewAlbumForm.jsx
@@ -41,9 +41,9 @@ export default function NewAlbumForm({category, hidden, setOpen}) {
     for (let i = 0; i < photosRef.current.files.length; i++) {
       newFormData.append('photos', photosRef.current.files[i])
     };
-    // photosRef.current.files.forEach((f) => newFormData.append('photos', f));
-    // newFormData.append('photos', photosRef.current.files);
     await create(newFormData);
+    thumbnailRef.current.value = '';
+    photosRef.current.value = '';
     setFormData(baseData);
     setLoading(false);
   }
@@ -53,12 +53,12 @@ export default function NewAlbumForm({category, hidden, setOpen}) {
       <button onClick={handleClose} id='close-album-form-btn'>X</button>
       <div id='new-album-container'>
         <form id='album-form' onSubmit={handleSubmit}>
-          <input id='title-input' name='title' type="text" placeholder='Enter Title' onChange={handleChange}/>
+          <input id='title-input' name='title' type="text" placeholder='Enter Title' onChange={handleChange} value={formData.title} />
           <label htmlFor="thumbnail">Thumbnail Image:</label>
           <input ref={thumbnailRef} id='thumbnail' name='thumbnail' type="file" placeholder='Title'/>
           <label htmlFor="photos">Select Photos:</label>
           <input ref={photosRef} id='photos' name='photos' type="file"  placeholder='Title' multiple/>
-          <button id='album-submit-button' type='submit'>SUBMIT</button>
+          <button disabled={isLoading} id='album-submit-button' type='submit'>{isLoading ? 'Creating Album...' : 'SUBMIT'}</button>
         </form>
       </div>
     </div>
