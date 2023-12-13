1e71d67dc9f6ebcff257db423bf2847103b74483
Alex Petit <alexpetit6@gmail.com>
2023-12-12 17:21:39 -0800

    added scroll btn to landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,14 +1,25 @@
 import './Landing.css';
+import React, { useRef } from 'react';
 import Line from '../../components/Line/Line';
 
 export default function Landing () {
-  // ●
+  const landingRef = useRef(null);
+
+  function scroll() {
+    const portPos = landingRef.current.getBoundingClientRect().bottom;
+    window.scrollBy({
+      top: portPos,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
 
   return (
-    <div id='landing-bg'>
+    <div ref={landingRef} id='landing-bg'>
       <h1 id='landing-title'>Katie Newbury</h1>
       <Line />
       <h3 id='landing-subtitle'>Choreographer ● Actor ● Artist</h3>
+      <i onClick={scroll} className="fa-solid fa-chevron-up"></i>
     </div>
   )
 }
